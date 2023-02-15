551050a54b5ef478d56b805ab519d1c8fdf9e99f
Denis Lavrov <bahus.vel@gmail.com>
2023-02-15 17:35:09 +1300

    Added dictionary training function

---
+++ zdict.go
@@ -0,0 +1,35 @@
+package zstd
+
+/*
+#define ZDICT_STATIC_LINKING_ONLY
+#include "zdict.h"
+*/
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func TrainFromBuffer(samples [][]byte) ([]byte, error) {
+	nbSamples := C.unsigned(len(samples))
+	sampleSizes := make([]C.size_t, len(samples))
+	var dictBufferCapacity C.size_t = 0
+	for i, sample := range samples {
+		sampleSizes[i] = C.size_t(len(sample))
+		dictBufferCapacity += C.size_t(len(sample))
+	}
+	samplesBuffer := make([]byte, dictBufferCapacity)
+	offset := 0
+	for _, sample := range samples {
+		copy(samplesBuffer[offset:offset+len(sample)], sample)
+		offset += len(sample)
+	}
+	dictBuffer := make([]byte, dictBufferCapacity)
+
+	dictSize := C.ZDICT_trainFromBuffer(unsafe.Pointer(&dictBuffer[0]), dictBufferCapacity, unsafe.Pointer(&samplesBuffer[0]), (*C.size_t)(unsafe.Pointer(&sampleSizes[0])), nbSamples)
+	if C.ZDICT_isError(dictSize) != 0 {
+		return []byte{}, fmt.Errorf("training failed %d", dictSize)
+	}
+
+	return dictBuffer[:dictSize], nil
+}
